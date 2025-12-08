efe0955395c148cbb399f258d74933723b5e0466
unknown <mustufabaigwastaken@gmail.com>
2025-12-08 13:43:18 +0530

    homepage created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import webbrowser
 import uvicorn
 import Developer_GPT
@@ -10,6 +11,15 @@ app=FastAPI()
 def build_app(brief: str):
     code=Developer_GPT.generate_app(brief)
     return {'code':code}
+
+
+@app.get("/")
+def home():
+	html=""
+	with open('file.html','r') as file:
+		html=file.read()
+
+	return HTMLResponse(content=html)
 
 
 if __name__ == "__main__":
